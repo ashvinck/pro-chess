f60b7e5474b65ae011482363d8a645701742b180
ashvinck <ashvinck10@gmail.com>
2023-09-29 20:10:30 +0530

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,57 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {
+  CssBaseline,
+  Paper,
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material';
+import { customizations } from './theme';
+import styled from '@emotion/styled';
 
 function App() {
+  let theme = createTheme(customizations());
+  theme = responsiveFontSizes(theme);
+
+  const StyledPaper = styled(Paper)(() => ({
+    minHeight: '100vh',
+    minWidth: '100vw',
+    borderRadius: '0px',
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <StyledPaper elevation={0}>
+        <div className='App'>
+          <h1>Hello !</h1>
+        </div>
+      </StyledPaper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const customizations = () => ({
+  palette: {
+    primary: {
+      main: '#181818',
+      light: '#54545c',
+      dark: '#191919',
+    },
+    secondary: {
+      main: '#c8b5a7',
+    },
+  },
+  typography: {
+    fontFamily: 'Poppins, Dela Gothic One, cursive, sans-serif',
+  },
+});
