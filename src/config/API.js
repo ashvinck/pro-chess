24925948e3105a761662e984851b178869f7c82c
ashvinck <ashvinck10@gmail.com>
2024-12-18 08:44:40 +0530

    render deployment

--- src/config/API.js
+++ src/config/API.js
@@ -1,3 +1,3 @@
 // export const API = 'http://localhost:4000';
-export const API = 'https://pro-chess-backend.vercel.app';
-// export const API = 'https://prochess.onrender.com';
+// export const API = 'https://pro-chess-backend.vercel.app';
+export const API = 'https://prochess.onrender.com';
